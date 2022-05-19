ec51337b54bca23f8b3bfd09c77f59b914ca994c
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-05-19 19:09:40 -0300

    Update contato.js

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -10,9 +10,68 @@ var verificaAutenticacao = require('../../config/auth');
 
 module.exports = function (app) {
     var controller = app.controllers.contato;
+    var Contato = app.models.contato;
+
+    controller.listaContatos = function(req, res) {
+        Contato.find().exec().then(
+            function(contatos) {
+                res.json(contatos);
+            },
+            function(erro) {
+                console.error(erro)
+                res.status(500).json(erro);
+            });
+    };
+
+    controller.obtemContato = function(req, res) {
+        var _id = req.params.id;
+        Contato.findById(_id).exec().then(
+            function(contato) {
+                if (!contato) throw new Error("Contato não encontrado");
+                res.json(contato)
+            },
+            function(erro) {
+                console.log(erro);
+                res.status(404).json(erro)
+            });
+    };
+
+    controller.removeContato = function(req, res) {
+        var _id = req.params.id;
+        Contato.deleteOne({ "_id": _id }).exec().then(
+            function() {
+                res.end();
+            },
+            function(erro) {
+                return console.error(erro);
+            });
+    };
+
+    controller.salvaContato = function(req, res) {
+        var _id = req.body._id;
+        if (_id) {
+            Contato.findByIdAndUpdate(_id, req.body).exec().then(
+                function(contato) {
+                    res.json(contato);
+                },
+                function(erro) {
+                    console.error(erro)
+                    res.status(500).json(erro);
+                });
+        } else {
+            Contato.create(req.body).then(
+                function(contato) {
+                    res.status(201).json(contato);
+                },
+                function(erro) {
+                    console.log(erro);
+                    res.status(500).json(erro);
+                });
+        }
+    };
 
     app.route('/contatos')
-        .get(verificaAutenticacao, controller.listaTodos)
+        .get(verificaAutenticacao, controller.listaContatos)
         .post(verificaAutenticacao, controller.salvaContato);
 
     app.route('/contatos/:id')

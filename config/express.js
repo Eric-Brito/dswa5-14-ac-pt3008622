a46ef90c55a3f96f47ac9d1abeafff43a7eaaef6
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-05-26 19:31:35 -0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -44,6 +44,10 @@ module.exports = function() {
 
     //Carregar pastas
     load('models', { cwd: 'app' }).then('controllers').then('routes/auth.js').then('routes').into(app);
-
+    
+    app.get('*', function(req, res) {
+       res.status(404).render('404'); 
+    });
+    
     return app;
 };

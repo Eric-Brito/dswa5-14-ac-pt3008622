b90775e68ca8ec8989d2e6027dba9a7488d5279b
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-06-02 13:26:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var http = require('http');
-var express = require('express');
-var app = express();
-require('./config/express')(app);
+var app = require('./config/express')();
 require('./config/passport')();
-require('./config/database')('mongodb+srv://dswa5:dswa5@clusterone.g7xlk.mongodb.net/test');
+require('./config/database')('mongodb+srv://dswa5:dswa5@clusterone.g7xlk.mongodb.net/ifsp?retryWrites=true&w=majority');
+require('./config/database.js')(url);
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express Server escutando na porta ' + app.get('port'));

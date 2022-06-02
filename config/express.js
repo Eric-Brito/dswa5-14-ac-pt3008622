75e5c0c61a82f969d8c58970ae025fbebce0c2f1
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-06-02 13:50:10 -0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var load = require('express-load');
+
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
-helmet = require('helmet')
 
 module.exports = function() {
     //Instância do Express
@@ -12,7 +12,7 @@ module.exports = function() {
 
     //Porta da aplicação	
     //app.set('port', 3000);
-    app.set('port', process.env.PORT || 3000);
+    app.set('port', process.env.PORT || 5000);
 
     //Middleware
     app.use(express.static('./public'));
@@ -20,34 +20,27 @@ module.exports = function() {
     app.use(bodyParser.json());
     app.use(require('method-override')());
 
-    //Novos middlewares
-    app.use(cookieParser());
-    app.use(session(
-        { secret: 'homem avestruz',
-          resave: true,
-          saveUninitialized: true
-        }
-    ));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    
-    app.use(helmet());
-    app.disable('x-powered-by');
-    app.use(helmet.hidePoweredBy({ setTo: 'PHP 5.5.14' }));
-    app.use(helmet.xframe());
-    app.use(helmet.xssFilter());
-    app.use(helmet.nosniff());
-
     //Definir Engine para a View
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
 
+    app.use(cookieParser());
+    app.use(session({
+        secret: 'campus pirituba',
+        resave: true,
+        saveUninitialized: true
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+
     //Carregar pastas
-    load('models', { cwd: 'app' }).then('controllers').then('routes/auth.js').then('routes').into(app);
-    
-    app.get('*', function(req, res) {
-       res.status(404).render('404'); 
-    });
-    
+    load('models', { cwd: 'app' })
+        .then('controllers')
+        .then('routes/auth.js')
+        .then('routes')
+        .into(app);
+
     return app;
 };

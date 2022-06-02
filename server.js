681b725b15615053bc298340dfb8a0db5663df46
Eric Brito <entersinisc2@gmail.com>
2022-06-02 20:18:14 -0300

    yes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var app = require('./config/express')();
 require('./config/passport')();
 require('./config/database')('mongodb+srv://dswa5:dswa5@clusterone.g7xlk.mongodb.net/ifsp?retryWrites=true&w=majority');
-require('./config/database.js')(mongodb://localhost/DSWA533);
+require('./config/database.js')('mongodb://localhost/DSWA533');
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express Server escutando na porta ' + app.get('port'));
